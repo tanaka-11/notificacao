7f6ccd20d2d2c8f7cdb48e94b812453ab97e0ffa
Marina Tanaka <mtanakasg@gmail.com>
2024-04-02 10:56:12 -0300

    estrutura inicial, importações e função enviarNotificação

--- App.js
+++ App.js
@@ -1,12 +1,57 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, StatusBar, Button } from "react-native";
+import { useState, useEffect } from "react";
+
+// expo-notifications
+import * as Notifications from "expo-notifications";
+
+// Manipulando o evento de notificação
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: false,
+    };
+  },
+});
 
 export default function App() {
+  // State da notificação
+  const [dados, setDados] = useState(null);
+
+  // Effect de permissões
+  useEffect(() => {
+    // Função de permissão IOS
+    async function permissoesIOS() {
+      return await Notifications.requestPermissionsAsync({
+        ios: {
+          allowAlert: true,
+          allowSound: true,
+          allowBadge: true,
+        },
+      });
+    }
+    permissoesIOS();
+
+    // Alert no topo do aplicativo
+    Notifications.addNotificationReceivedListener((notificacao) => {
+      console.log(notificacao);
+    });
+
+    // Interação do usuario com a notificação
+  }, []);
+
+  // Função para enviar notificação
+  const enviarNotificacao = () => {};
+
   return (
-    <View style={styles.container}>
-      <Text>Recurso de Notificação</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <View style={styles.container}>
+        <Text>Recurso de Notificação Local</Text>
+        <Button title="Disparar notificação" onPress={enviarNotificacao} />
+      </View>
+    </>
   );
 }
 
